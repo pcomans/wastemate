11b162520a2562ba05a8faadda7a9f4424c2f41b
Philipp Comans <philipp.comans@gmail.com>
2018-04-04 18:00:27 -0700

    Split into description and search terms

--- recology.dart
+++ recology.dart
@@ -1,79 +1,440 @@
 // Source: https://www.recology.com/recology-san-francisco/sorting-guides-and-signage/
 
-List<String> compost = [
-  "Bread, grains and pasta",
-  "Coffee grounds with paper filter",
-  "Dairy",
-  "Eggshells and eggs",
-  "Fruit (pits & peels)",
-  "Leftovers and spoiled food",
-  "Meat (including bones)",
-  "Seafood (including shellfish)",
-  "Tea and tea bags",
-  "Vegetables",
-  "Coffee filters",
-  "Greasy pizza boxes",
-  "Paper plates",
-  "Paper bags, napkins, tissues and towels",
-  "Paper take-out boxes and containers (metal handle OK)",
-  "Tissues",
-  "Branches and brush",
-  "Flowers and floral trimmings",
-  "Grasses and weeds",
-  "Leaves",
-  "Tree trimmings (less than 6 inches in diameter and 4 feet long)",
-  "Cotton balls and cotton swabs",
-  "Hair, fur, and feathers (non-synthetic)",
-  "Plastic and cutlery clearly labeled “Compostable”",
-  "(green stripe or sticker to allow for easy identification)",
-  "Vegetable wood crates (metal wire is okay)",
-  "Waxed cardboard",
-  "Wood – small pieces of lumber or sawdust from clean wood only (no plywood, press board, painted, stained or treated wood)",
-  "Wooden chop sticks",
-  "Corks – natural (drop in barrels at Whole Foods)",
+List<Map<String, dynamic>> compost = [
+  {
+    "terms": ["bread", "grains", "pasta"],
+    "desc": "Bread, grains and pasta",
+  },
+  {
+    "terms": ["coffee grounds"],
+    "desc": "Coffee grounds with paper filter",
+  },
+  {
+    "terms": ["dairy", "cheese", "yoghurt"],
+    "desc": "Dairy",
+  },
+  {
+    "terms": ["eggshells", "eggs"],
+    "desc": "Eggshells and eggs",
+  },
+  {
+    "terms": ["fruit", "apple", "pear"],
+    "desc": "Fruit (pits & peels)",
+  },
+  {
+    "terms": [
+      "leftovers",
+      "food",
+      "spoiled food",
+    ],
+    "desc": "Leftovers and spoiled food",
+  },
+  {
+    "terms": [
+      "Meat",
+      "bones",
+    ],
+    "desc": "Meat (including bones)",
+  },
+  {
+    "terms": [
+      "Seafood",
+      "shellfish",
+    ],
+    "desc": "Seafood (including shellfish)",
+  },
+  {
+    "terms": [
+      "Tea",
+      "tea bags",
+    ],
+    "desc": "Tea and tea bags",
+  },
+  {
+    "terms": [
+      "Vegetables",
+    ],
+    "desc": "Vegetables",
+  },
+  {
+    "terms": [
+      "Coffee filters",
+    ],
+    "desc": "Coffee filters",
+  },
+  {
+    "terms": [
+      "Greasy pizza boxes",
+    ],
+    "desc": "Greasy pizza boxes",
+  },
+  {
+    "terms": [
+      "Paper plates",
+    ],
+    "desc": "Paper plates",
+  },
+  {
+    "terms": [
+      "Paper bags, napkins, tissues and towels",
+    ],
+    "desc": "Paper bags, napkins, tissues and towels",
+  },
+  {
+    "terms": [
+      "Paper take-out boxes and containers (metal handle OK)",
+    ],
+    "desc": "Paper take-out boxes and containers (metal handle OK)",
+  },
+  {
+    "terms": [
+      "Tissues",
+    ],
+    "desc": "Tissues",
+  },
+  {
+    "terms": [
+      "Branches and brush",
+    ],
+    "desc": "Branches and brush",
+  },
+  {
+    "terms": [
+      "Flowers and floral trimmings",
+    ],
+    "desc": "Flowers and floral trimmings",
+  },
+  {
+    "terms": [
+      "Grasses and weeds",
+    ],
+    "desc": "Grasses and weeds",
+  },
+  {
+    "terms": [
+      "Leaves",
+    ],
+    "desc": "Leaves",
+  },
+  {
+    "terms": [
+      "Tree trimmings (less than 6 inches in diameter and 4 feet long)",
+    ],
+    "desc": "Tree trimmings (less than 6 inches in diameter and 4 feet long)",
+  },
+  {
+    "terms": [
+      "Cotton balls and cotton swabs",
+    ],
+    "desc": "Cotton balls and cotton swabs",
+  },
+  {
+    "terms": [
+      "Hair, fur, and feathers (non-synthetic)",
+    ],
+    "desc": "Hair, fur, and feathers (non-synthetic)",
+  },
+  {
+    "terms": [
+      "Plastic and cutlery clearly labeled “Compostable”",
+    ],
+    "desc": "Plastic and cutlery clearly labeled “Compostable”",
+  },
+  {
+    "terms": [
+      "(green stripe or sticker to allow for easy identification)",
+    ],
+    "desc": "(green stripe or sticker to allow for easy identification)",
+  },
+  {
+    "terms": [
+      "Vegetable wood crates (metal wire is okay)",
+    ],
+    "desc": "Vegetable wood crates (metal wire is okay)",
+  },
+  {
+    "terms": [
+      "Waxed cardboard",
+    ],
+    "desc": "Waxed cardboard",
+  },
+  {
+    "terms": [
+      "Wood – small pieces of lumber or sawdust from clean wood only (no plywood, press board, painted, stained or treated wood)",
+    ],
+    "desc":
+        "Wood – small pieces of lumber or sawdust from clean wood only (no plywood, press board, painted, stained or treated wood)",
+  },
+  {
+    "terms": [
+      "Wooden chop sticks",
+    ],
+    "desc": "Wooden chop sticks",
+  },
+  {
+    "terms": [
+      "Corks – natural (drop in barrels at Whole Foods)",
+    ],
+    "desc": "Corks – natural (drop in barrels at Whole Foods)",
+  },
 ];
 
-List<String> recycling = [
-  "Aluminum cans",
-  "Aluminum foil and trays (ball foil up to softball size)",
-  "Caps and lids from bottles, jars and steel (tin) cans",
-  "Paint cans (must be empty or dry)",
-  "Spray cans (must be empty)",
-  "Steel (tin) cans",
-  "Clean, dry, empty plastic bags inside a clear plastic bag (the size roughly of a basketball)",
-  "Bottles (leave caps on)",
-  "Buckets (metal handle ok)",
-  "CD’s, DVD’s, CDROM & Cases (remove paper insert)",
-  "Coffee cup lids",
-  "Containers and clamshells",
-  "Corks – plastic",
-  "Cups and plates (plastic only, no Styrofoam)",
-  "Flower pots and trays – plastic",
-  "Laundry detergent bottles",
-  "Molded plastic packaging",
-  "Toys (no electronics, metal or batteries)",
-  "Tubs and lids (i.e.,: yogurt containers and Tupperware)",
-  "Utensils – plastic",
-  "Linens – Clean and placed in clear bag. Towels, curtains okay.",
-  "Fabric – Clean and placed in clear bag. Rips, holes, single socks okay. No sneakers, boots, belts, or purses.",
-  "Rags – Clean and placed in clear bag. Towels, curtains okay.",
-  "Clothes – Rips, holes, single socks okay. Place clean clothes in clear bag. If clothes are in good condition, please donate them to a thrift store.",
-  "Empty, clean coffee cups",
-  "Empty, clean ice cream containers",
-  "Soup cartons and juice boxes",
-  "Paper milk, juice and other beverage cartons",
-  "Bags (paper only)",
-  "Cardboard (non-waxed)",
-  "Cereal boxes and Paperboard (remove plastic liner)",
-  "Computer and office paper",
-  "Egg cartons (paper)",
-  "Envelopes (windows okay)",
-  "Junk mail and magazines",
-  "Newspapers",
-  "Packing or Kraft paper",
-  "Phonebooks",
-  "Sticky notes",
-  "Shredded paper (place in sealed paper bag and label “Shredded Paper”)",
-  "Wrapping paper (non-metallic)",
-  "Glass bottles and jars only (metal caps and lids too)",
+List<Map<String, dynamic>> recycling = [
+  {
+    "terms": [
+      "Aluminum cans",
+    ],
+    "desc": "Aluminum cans",
+  },
+  {
+    "terms": [
+      "Aluminum foil",
+      "Aluminum trays",
+    ],
+    "desc": "Aluminum foil and trays (ball foil up to softball size)",
+  },
+  {
+    "terms": [
+      "Caps and lids from bottles, jars and steel (tin) cans",
+    ],
+    "desc": "Caps and lids from bottles, jars and steel (tin) cans",
+  },
+  {
+    "terms": [
+      "Paint cans (must be empty or dry)",
+    ],
+    "desc": "Paint cans (must be empty or dry)",
+  },
+  {
+    "terms": [
+      "Spray cans (must be empty)",
+    ],
+    "desc": "Spray cans (must be empty)",
+  },
+  {
+    "terms": [
+      "Steel (tin) cans",
+    ],
+    "desc": "Steel (tin) cans",
+  },
+  {
+    "terms": [
+      "Clean, dry, empty plastic bags inside a clear plastic bag (the size roughly of a basketball)",
+    ],
+    "desc":
+        "Clean, dry, empty plastic bags inside a clear plastic bag (the size roughly of a basketball)",
+  },
+  {
+    "terms": [
+      "Bottles (leave caps on)",
+    ],
+    "desc": "Bottles (leave caps on)",
+  },
+  {
+    "terms": [
+      "Buckets (metal handle ok)",
+    ],
+    "desc": "Buckets (metal handle ok)",
+  },
+  {
+    "terms": [
+      "CD’s, DVD’s, CDROM & Cases (remove paper insert)",
+    ],
+    "desc": "CD’s, DVD’s, CDROM & Cases (remove paper insert)",
+  },
+  {
+    "terms": [
+      "Coffee cup lids",
+    ],
+    "desc": "Coffee cup lids",
+  },
+  {
+    "terms": [
+      "Containers and clamshells",
+    ],
+    "desc": "Containers and clamshells",
+  },
+  {
+    "terms": [
+      "Corks – plastic",
+    ],
+    "desc": "Corks – plastic",
+  },
+  {
+    "terms": [
+      "Cups and plates (plastic only, no Styrofoam)",
+    ],
+    "desc": "Cups and plates (plastic only, no Styrofoam)",
+  },
+  {
+    "terms": [
+      "Flower pots and trays – plastic",
+    ],
+    "desc": "Flower pots and trays – plastic",
+  },
+  {
+    "terms": [
+      "Laundry detergent bottles",
+    ],
+    "desc": "Laundry detergent bottles",
+  },
+  {
+    "terms": [
+      "Molded plastic packaging",
+    ],
+    "desc": "Molded plastic packaging",
+  },
+  {
+    "terms": [
+      "Toys (no electronics, metal or batteries)",
+    ],
+    "desc": "Toys (no electronics, metal or batteries)",
+  },
+  {
+    "terms": [
+      "Tubs and lids (i.e.,: yogurt containers and Tupperware)",
+    ],
+    "desc": "Tubs and lids (i.e.,: yogurt containers and Tupperware)",
+  },
+  {
+    "terms": [
+      "Utensils – plastic",
+    ],
+    "desc": "Utensils – plastic",
+  },
+  {
+    "terms": [
+      "Linens – Clean and placed in clear bag. Towels, curtains okay.",
+    ],
+    "desc": "Linens – Clean and placed in clear bag. Towels, curtains okay.",
+  },
+  {
+    "terms": [
+      "Fabric – Clean and placed in clear bag. Rips, holes, single socks okay. No sneakers, boots, belts, or purses.",
+    ],
+    "desc":
+        "Fabric – Clean and placed in clear bag. Rips, holes, single socks okay. No sneakers, boots, belts, or purses.",
+  },
+  {
+    "terms": [
+      "Rags – Clean and placed in clear bag. Towels, curtains okay.",
+    ],
+    "desc": "Rags – Clean and placed in clear bag. Towels, curtains okay.",
+  },
+  {
+    "terms": [
+      "Clothes – Rips, holes, single socks okay. Place clean clothes in clear bag. If clothes are in good condition, please donate them to a thrift store.",
+    ],
+    "desc":
+        "Clothes – Rips, holes, single socks okay. Place clean clothes in clear bag. If clothes are in good condition, please donate them to a thrift store.",
+  },
+  {
+    "terms": [
+      "Empty, clean coffee cups",
+    ],
+    "desc": "Empty, clean coffee cups",
+  },
+  {
+    "terms": [
+      "Empty, clean ice cream containers",
+    ],
+    "desc": "Empty, clean ice cream containers",
+  },
+  {
+    "terms": [
+      "Soup cartons and juice boxes",
+    ],
+    "desc": "Soup cartons and juice boxes",
+  },
+  {
+    "terms": [
+      "Paper milk, juice and other beverage cartons",
+    ],
+    "desc": "Paper milk, juice and other beverage cartons",
+  },
+  {
+    "terms": [
+      "Bags (paper only)",
+    ],
+    "desc": "Bags (paper only)",
+  },
+  {
+    "terms": [
+      "Cardboard (non-waxed)",
+    ],
+    "desc": "Cardboard (non-waxed)",
+  },
+  {
+    "terms": [
+      "Cereal boxes and Paperboard (remove plastic liner)",
+    ],
+    "desc": "Cereal boxes and Paperboard (remove plastic liner)",
+  },
+  {
+    "terms": [
+      "Computer and office paper",
+    ],
+    "desc": "Computer and office paper",
+  },
+  {
+    "terms": [
+      "Egg cartons (paper)",
+    ],
+    "desc": "Egg cartons (paper)",
+  },
+  {
+    "terms": [
+      "Envelopes (windows okay)",
+    ],
+    "desc": "Envelopes (windows okay)",
+  },
+  {
+    "terms": [
+      "Junk mail and magazines",
+    ],
+    "desc": "Junk mail and magazines",
+  },
+  {
+    "terms": [
+      "Newspapers",
+    ],
+    "desc": "Newspapers",
+  },
+  {
+    "terms": [
+      "Packing or Kraft paper",
+    ],
+    "desc": "Packing or Kraft paper",
+  },
+  {
+    "terms": [
+      "Phonebooks",
+    ],
+    "desc": "Phonebooks",
+  },
+  {
+    "terms": [
+      "Sticky notes",
+    ],
+    "desc": "Sticky notes",
+  },
+  {
+    "terms": [
+      "Shredded paper (place in sealed paper bag and label “Shredded Paper”)",
+    ],
+    "desc":
+        "Shredded paper (place in sealed paper bag and label “Shredded Paper”)",
+  },
+  {
+    "terms": [
+      "Wrapping paper (non-metallic)",
+    ],
+    "desc": "Wrapping paper (non-metallic)",
+  },
+  {
+    "terms": [
+      "Glass bottles and jars only (metal caps and lids too)",
+    ],
+    "desc": "Glass bottles and jars only (metal caps and lids too)",
+  },
 ];
--- v1.dart
+++ v1.dart
@@ -1,16 +1,33 @@
 import "recology.dart";
 
 void main() {
-  print(whereToPut("box"));
+  print(whereToPut("coffee grounds"));
+}
+
+List<String> matchStruct(String itemName, List<Map<String, dynamic>> structs) {
+  List<String> matches = [];
+  structs.forEach((Map<String, dynamic> c) {
+    List<String> terms = c["terms"];
+    String desc = c["desc"];
+    String matchTerms = terms.firstWhere(
+      (String t) => t.toLowerCase().contains(itemName.toLowerCase()),
+      orElse: () => null,
+    );
+    if (matchTerms != null) {
+      matches.add(desc);
+    }
+  });
+  return matches;
 }
 
 String whereToPut(String itemName) {
-  List<String> matchCompost = compost.where((c) => c.toLowerCase().contains(itemName.toLowerCase()));
-  List<String> matchRecycling = recycling.where((c) => c.toLowerCase().contains(itemName.toLowerCase()));
+  List<String> matchCompost = matchStruct(itemName, compost);
+
+  List<String> matchRecycling = matchStruct(itemName, recycling);
 
   String response = "";
   if (matchCompost.length == 0 && matchRecycling.length == 0) {
-    response = "You can't recycle or compost that.";
+    response = "I don't know if you can recycle or compost ${itemName}.";
   }
   if (matchCompost.length > 0) {
     response += "You can compost ${matchCompost.join(" and ")}. ";
